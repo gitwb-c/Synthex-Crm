8a3efe097238ee36c13b71e7457acdc1b2335f33
Vinicius Guarnieri <svini0966@gmail.com>
2025-10-27 15:01:15 -0300

    first commit

---
+++ frontend/src/routes/company/[companyId]/etapa/[pipelineId]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { PageProps } from './$types';
+
+    let { data, params }: PageProps = $props();
+    const {pipelineId} = params
+</script>
+
+<h1>Etapa {pipelineId}</h1>
